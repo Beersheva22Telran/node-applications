f56bff12d374ad411f4bbb9ff431a620cebb2251
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-12 10:57:27 +0300

    final code

--- calculator.js
+++ calculator.js
@@ -1,20 +1,49 @@
 import http from 'node:http';
 import config from 'config';
-import {operations} from './config/operations.js'
-import {URL, URLSearchParams} from 'url'
+import { operations } from './config/operations.js'
+import { URL } from 'url'
 import CalculatorService from './service/CalculatorService.js';
+import CalculatorView from './view/CalculatorView.js';
 const server = http.createServer();
 const port = process.env.PORT || config.has('server.port') && config.get('server.port') || 0;
 const calculatorService = new CalculatorService(server, operations);
+
+const view = new CalculatorView();
 server.listen(port, () => console.log(`server is listening on the port ${server.address().port}`))
 server.on('request', (req, res) => {
    res.setHeader('content-type', 'text/html')
-    const reqUrl = new URL(`http://${req.headers.host}${req.url}`);
-    let result;
-    const operands = reqUrl.searchParams;
-    const op1 = +operands.get('op1');
-    const op2 = +operands.get('op2');
-   server.emit('/add', [op1, op2], res);
-    
-    
+   const reqUrl = new URL(`http://${req.headers.host}${req.url}`);
+   if (!operations.has(reqUrl.pathname)) {
+      writeError(reqUrl.pathname + " Unknown operation", res);
+   } else {
+      const operands = [];
+      const error = getOperands(reqUrl, operands);
+      if (error) {
+         writeError(error, res);
+      } else {
+         server.emit(reqUrl.pathname, operands, res);
+      }
+   }
 })
+
+function getOperands(reqUrl, operandsAr) {
+   const operands = reqUrl.searchParams;
+   const op1 = operands.get('op1');
+   const op2 = operands.get('op2');
+   let res = "";
+   if (op1 == undefined || isNaN(+op1)) {
+      res = `Wrong or not exists op1 operand ;`;
+   } 
+    if(op2 == undefined || isNaN(+op2)) {
+      res += `Wrong or not exists op2 operand`
+   }
+   if (!res) {
+      operandsAr.push(+op1, +op2);
+   }
+   return res;
+}
+function writeError(error, response) {
+   response.write(view.getHtml(error, true));
+   response.end();
+}
+
--- service/CalculatorService.js
+++ service/CalculatorService.js
@@ -3,9 +3,11 @@ import CalculatorView from "../view/CalculatorView.js";
 const view = new CalculatorView();
 export default class CalculatorService {
     constructor(emitter, operations) {
-        emitter.on("/add", (operands,response) => {
-           this.writeResult(operands[0] + operands[1], response)
-        }) 
+       
+       Array.from(operations.entries())
+       .forEach(operation => emitter.addListener(operation[0], (operands, response) => {
+        this.writeResult(operation[1](operands[0], operands[1]), response)
+       }))
     }
     writeResult(result, response) {
         response.write(view.getHtml(result, false));
