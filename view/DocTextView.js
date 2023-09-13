fccbcaf5e1f477be4867465a881b758307705ef9
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-13 13:27:36 +0300

    ModoDb connection

--- view/DocTextView.js
+++
@@ -1,8 +0,0 @@
-export class DocTextView {
-    renderLine(line) {
-        return `<label style="font-size:1.5em;font-style:italic;display:block;text-align:center">${line}</label>`
-    }
-    renderError(error, response) {
-        response.end(`<label style="font-size:1.5em;color:red;display:block;text-align:center">${error}</label>`)
-    }
-}
