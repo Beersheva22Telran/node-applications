9189843a66eb487059e62bbbd48d8acfeb7bc811
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-18 18:55:14 +0300

    final code with no websocket

--- routes/employees.mjs
+++ routes/employees.mjs
@@ -1,10 +1,23 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler'
 import EmployeesService from '../service/EmployeesService.mjs';
+import { validate } from "../middleware/validation.mjs";
+import config from 'config'
+import authVerification from "../middleware/authVerification.mjs";
+import Joi from 'joi'
 export const employees = express.Router();
 const employeesService = new EmployeesService();
-//TODO validation schema based on Joi
-employees.delete('/:id', asyncHandler(
+const {minId, maxId, minDate, maxDate, departments, minSalary, maxSalary} = config.get('employee');
+const schema = Joi.object({
+    id: Joi.number().min(minId).max(maxId),
+    birthDate: Joi.date().iso().less(maxDate).greater(minDate).required(),
+    name: Joi.string().required(),
+    department: Joi.any().allow(...departments).required(),
+    salary: Joi.number().min(minSalary).max(maxSalary).required(),
+    gender: Joi.any().allow('male', 'female').required()
+})
+employees.use(validate(schema));
+employees.delete('/:id', authVerification("ADMIN"), asyncHandler(
     async (req, res) => {
         const id = +req.params.id;
         if (!await employeesService.deleteEmployee(id)){
@@ -13,4 +26,64 @@ employees.delete('/:id', asyncHandler(
         }
         res.send();
     }
+))
+
+employees.post('',authVerification("ADMIN"), asyncHandler(
+    async (req, res) => {
+
+        if(req.joiError) {
+            res.status(400);
+            throw req.joiError;
+        }
+        const employee = await employeesService.addEmployee(req.body);
+        if (!employee && req.body.id) {
+            res.status(400);
+            throw `employee with id ${req.body.id} already exists`
+        }
+        res.send(employee);
+    }
+))
+employees.put('/:id',authVerification("ADMIN"), asyncHandler(
+    async (req, res) => {
+        if(req.joiError) {
+            res.status(400);
+            throw req.joiError;
+        }
+        if (req.params.id != req.body.id) {
+            res.status(400);
+            throw `id in request parameter (${req.params.id}) doesn't match the id in employee object (req.body.id)`
+        }
+        const employee = await employeesService.updateEmployee(req.body);
+        if (!employee) {
+            res.status(404);
+            throw `employee with id ${req.body.id} doesn't exist`
+        }
+        res.send(employee);
+    }
+))
+employees.delete('/:id',authVerification("ADMIN"), asyncHandler(
+    async (req,res) => {
+        if(!await employeesService.deleteEmployee(+req.params.id)) {
+            res.status(404);
+            throw `employee with id ${req.params.id} doesn't exist`
+        }
+        res.send();
+    }
+))
+ employees.get('', authVerification("ADMIN", "USER"),asyncHandler(
+    async(req,res) => {
+        const employees = await employeesService.getAllEmployees();
+        res.send(employees);
+    }
+))
+employees.get('/:id',authVerification("ADMIN", "USER"), asyncHandler(
+   async (req, res) => {
+    
+    const employee = await employeesService.getEmployee(+req.params.id);
+    if (!employee) {
+        res.status(404);
+        throw `employee with id ${req.params.id} doesn't exist`
+    }
+    res.send(employee)
+   } 
 ))
